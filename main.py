dd1c59e6f1965453fc4a96b4b1f44424cdb35f4e
mbolot <mbolot@DESKTOP-GGDQB08.attlocal.net>
2020-03-04 19:41:47 -0600

    Formatting cleanup

--- main.py
+++ main.py
@@ -2,39 +2,34 @@ from pyspark import SparkConf, SparkContext
 
 from sys import argv
 
-
-
-#common initialization for items used by the entire script
+# common initialization for items used by the entire script
 
 conf = (SparkConf()
         .setMaster("local")
         .setAppName("John EMR Test")
         )
 
-sc = SparkContext(conf = conf)
+sc = SparkContext(conf=conf)
 
 
-def number_of_records(file_name):
-    text_file_rdd = sc.textFile(file_name)
+def number_of_records(target_file):
+    text_file_rdd = sc.textFile(target_file)
     return text_file_rdd.count()
 
 
-
 def log(output):
-    #logger function for controlled output
-    #Don't use print, use this instead
+    # logger function for controlled output
+    # Don't use print, use this instead
     with open('logs/logfile.txt', 'a') as outfile:
         outfile.write(output)
         outfile.write('\n')
 
 
-
 def clear_resources():
-    #Clears the common resources used by the program
-    #Just the log file for now
+    # Clears the common resources used by the program
+    # Just the log file for now
     with open('logs/logfile.txt', 'w+') as outfile:
         outfile.write('')
-
 
 
 if __name__ == "__main__":
@@ -45,8 +40,8 @@ if __name__ == "__main__":
     clear_resources()
     file_name = argv[1]
     split_file_name = file_name.split('.')
-    extension = split_file_name[len(split_file_name)-1]
+    extension = split_file_name[len(split_file_name) - 1]
     if extension != 'tsv':
-        log("Warning: This program is desgined to process tsv files, other file types are unsupported")
+        log("Warning: This program is designed to process tsv files, other file types are unsupported")
 
     log(str(number_of_records(file_name)))
