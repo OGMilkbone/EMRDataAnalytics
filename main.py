866d834ddf0d76292138b22a5b6101f8682290c2
OGMilkbone <jwilson1@udallas.edu>
2020-03-04 21:28:16 -0600

    added get_title function

--- main.py
+++ main.py
@@ -35,6 +35,15 @@ def get_title(target_file):
     text_file_rdd = sc.textFile(target_file)
     return text_file_rdd.first()
 
+def review_analysis(target_file):
+    text_file_rdd = sc.textFile(target_file)
+    text_file_rdd = text_file_rdd.map(lambda x: x.split("\t"))
+    #remove header
+    text_file_rdd = text_file_rdd.filter(lambda x: x[0] == "marketplace")
+    text_file_rdd = text_file_rdd.map(lambda x: [x[5], x[7], x[12]]) #just grabs the important stuff
+    text_file_rdd = text_file_rdd.filter(lambda x: x[0] not in x[2])
+    text_file_rdd = text_file_rdd.map(lambda x: [x[1], 1]).countByKey()
+    return text_file_rdd
 
 
 
@@ -52,3 +61,4 @@ if __name__ == "__main__":
 
     log(str(number_of_records(file_name)))
     log(str(get_title(file_name)))
+    log(str(review_analysis(file_name)))
