9ca1b7abe5410c423bc533a0b4b16aadee89cc24
OGMilkbone <jwilson1@udallas.edu>
2020-03-04 21:50:01 -0600

    now that tings work lets change what we look for in the review body

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def review_analysis(target_file):
     #remove header
     text_file_rdd = text_file_rdd.filter(lambda x: x[0] != "marketplace")
     text_file_rdd = text_file_rdd.map(lambda x: [x[5], x[7], x[12]]) #just grabs the important stuff
-    text_file_rdd = text_file_rdd.filter(lambda x: x[0] in x[2])
+    text_file_rdd = text_file_rdd.filter(lambda x: "ugly" in x[2])
     text_file_rdd = text_file_rdd.map(lambda x: [x[1], 1]).countByKey()
     return text_file_rdd
 
